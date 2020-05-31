2c1156cd712d582aa561ea1e12c7d0121b1dff2d
Rimvydas Zilinskas <rimvydas.zilinskas@yahoo.com>
2020-05-31 17:36:21 +0200

    all the fixes

--- test/unit.test.js
+++ test/unit.test.js
@@ -7,7 +7,6 @@ const utils = require("../utils");
 
 const { expect, assert } = chai;
 
-
 describe("Test day of year", () => {
   it("Should return an integer", () => {
     let value = utils.getDayOfYear(new Date());
@@ -37,7 +36,6 @@ describe("Test day of year", () => {
   });
 });
 
-
 describe("Test leap is leap year", () => {
   given(
     {
@@ -61,110 +59,159 @@ describe("Test leap is leap year", () => {
   });
 });
 
-
 describe("Test type of horroscope", () => {
-    given(
-        {
-            date: new Date("2021-02-27"),
-            result: horrorscopes.bad[1],
-            value: 1,
-        },
-        {
-            date: new Date("2021-02-28"),
-            result: horrorscopes.bad[2],
-            value: 2
-        },
-        {
-            date: new Date("2021-03-01"),
-            result: horrorscopes.bad[3],
-            value: 3
-        },
-        {
-            date: new Date("2020-02-28"),
-            result: horrorscopes.good[0],
-            value: undefined
-        },
-        {
-            date: new Date("2020-02-29"),
-            result: horrorscopes.good[1],
-            value: undefined
-        },
-        {
-            date: new Date("2020-03-01"),
-            result: horrorscopes.good[0],
-            value: 0
-        },
-        {
-            date: new Date("2019-02-27"),
-            result: horrorscopes.bad[4],
-            value: 4
-        },
-        {
-            date: new Date("2019-02-28"),
-            result: horrorscopes.bad[5],
-            value: 5
-        },
-        {
-            date: new Date("2019-03-01"),
-            result: horrorscopes.bad[6],
-            value: 6
-        },
-    ).it("should return a horrorscope", (value) => {
-        expect(utils.getHoroscope(value.value, value.date)).to.be.equal(value.result);
-    })
+  given(
+    {
+      date: new Date("2021-02-27"),
+      result: horrorscopes.bad[1],
+      value: 1,
+    },
+    {
+      date: new Date("2021-02-28"),
+      result: horrorscopes.bad[2],
+      value: 2,
+    },
+    {
+      date: new Date("2021-03-01"),
+      result: horrorscopes.bad[3],
+      value: 3,
+    },
+    {
+      date: new Date("2020-02-28"),
+      result: horrorscopes.good[0],
+      value: undefined,
+    },
+    {
+      date: new Date("2020-02-29"),
+      result: horrorscopes.good[1],
+      value: undefined,
+    },
+    {
+      date: new Date("2020-03-01"),
+      result: horrorscopes.good[0],
+      value: 0,
+    },
+    {
+      date: new Date("2019-02-27"),
+      result: horrorscopes.bad[4],
+      value: 4,
+    },
+    {
+      date: new Date("2019-02-28"),
+      result: horrorscopes.bad[5],
+      value: 5,
+    },
+    {
+      date: new Date("2019-03-01"),
+      result: horrorscopes.bad[6],
+      value: 6,
+    }
+  ).it("should return a horrorscope", (value) => {
+    expect(utils.getHoroscope(value.value, value.date)).to.be.equal(
+      value.result
+    );
+  });
 });
 
 describe("Test Signs", () => {
   given(
-  {
-    date: new Date("2020-01-19"),
-    result: "Capricorn"
-  },
-  {
-    date: new Date("2020-02-19"),
-    result: "Aquarius"
-  },
-  {
-    date: new Date("2020-03-19"),
-    result: "Pisces"
-  }, 
-  {
-    date: new Date("2020-04-18"),
-    result: "Aries"
-  },
-  {
-    date: new Date("2020-05-19"),
-    result: "Taurus"
-  },
-  {
-    date: new Date("2020-06-19"),
-    result: "Gemini"
-  },
-  {
-    date: new Date("2020-07-19"),
-    result: "Cancer"
-  },
-  {
-    date: new Date("2020-08-19"),
-    result: "Leo"
-  },
-  {
-    date: new Date("2020-09-19"),
-    result: "Virgo"
-  },
-  {
-    date: new Date("2020-10-19"),
-    result: "Libra"
-  },
-  {
-    date: new Date("2020-11-19"),
-    result: "Scorpio"
-  },
-  {
-    date: new Date("2020-12-19"),
-    result: "Sagitarius"
-  }
-).it("Should return the correct sign", (value) => {
-  expect(utils.getSign(value.date)).to.be.equal(value.result);   
-});
+    {
+      date: new Date("2020-01-20"),
+      result: "Capricorn",
+    },
+    {
+      date: new Date("2020-12-22"),
+      result: "Capricorn",
+    },
+    {
+      date: new Date("2020-01-21"),
+      result: "Aquarius",
+    },
+    {
+      date: new Date("2020-02-18"),
+      result: "Aquarius",
+    },
+    {
+      date: new Date("2020-02-19"),
+      result: "Pisces",
+    },
+    {
+      date: new Date("2020-03-20"),
+      result: "Pisces",
+    },
+    {
+      date: new Date("2020-03-21"),
+      result: "Aries",
+    },
+    {
+      date: new Date("2020-04-20"),
+      result: "Aries",
+    },
+    {
+      date: new Date("2020-04-21"),
+      result: "Taurus",
+    },
+    {
+      date: new Date("2020-05-21"),
+      result: "Taurus",
+    },
+    {
+      date: new Date("2020-05-22"),
+      result: "Gemini",
+    },
+    {
+      date: new Date("2020-06-21"),
+      result: "Gemini",
+    },
+    {
+      date: new Date("2020-06-22"),
+      result: "Cancer",
+    },
+    {
+      date: new Date("2020-07-22"),
+      result: "Cancer",
+    },
+    {
+      date: new Date("2020-07-23"),
+      result: "Leo",
+    },
+    {
+      date: new Date("2020-08-23"),
+      result: "Leo",
+    },
+    {
+      date: new Date("2020-08-24"),
+      result: "Virgo",
+    },
+    {
+      date: new Date("2020-09-22"),
+      result: "Virgo",
+    },
+    {
+      date: new Date("2020-09-23"),
+      result: "Libra",
+    },
+    {
+      date: new Date("2020-10-23"),
+      result: "Libra",
+    },
+    {
+      date: new Date("2020-10-24"),
+      result: "Scorpio",
+    },
+    {
+      date: new Date("2020-11-22"),
+      result: "Scorpio",
+    },
+    {
+      date: new Date("2020-11-23"),
+      result: "Sagitarius",
+    },
+    {
+      date: new Date("2020-12-21"),
+      result: "Sagitarius",
+    }
+  ).it("Should return the correct sign", (value) => {
+    expect(utils.getSign(value.date)).to.be.equal(value.result);
+  });
 });
